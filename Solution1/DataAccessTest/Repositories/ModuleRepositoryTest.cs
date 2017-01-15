0440a69d4cb0001d1516ffec682e5f45b4dd2725
martafil <marta.dianna@gmail.com>
2017-01-15 08:14:04 +0200

    Modified Modules unit tests #45

--- Solution1/DataAccessTest/Repositories/ModuleRepositoryTest.cs
+++ Solution1/DataAccessTest/Repositories/ModuleRepositoryTest.cs
@@ -9,6 +9,8 @@ using FluentAssertions;
 using Microsoft.DotNet.ProjectModel;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccessTest.Repositories
 {
@@ -17,12 +19,23 @@ namespace DataAccessTest.Repositories
     {
         private IModuleRepository _moduleRepository;
         private Module _testModule;
+        private List<Module> expected_modules;
+        private List<Module> expected_updated_modules;
 
-        private Module CreateSUT()
+        private Module InitModule(string id)
         {
+            Guid module_id;
+            if (id == null)
+            {
+                module_id = Guid.Empty;
+            }
+            else
+            {
+                module_id = Guid.Parse(id);
+            }
             var newModule = new Module
             {
-                Id = Guid.Parse("0842f5b0-0ea3-4e16-143a-08d424682a17"),
+                Id = module_id,
                 CourseId = Guid.Empty,
                 Description = "Test Module",
                 HasHomework = true,
@@ -33,56 +46,79 @@ namespace DataAccessTest.Repositories
             return newModule;
         }
 
-        [TestInitialize]
-        public void SetUp()
+        private void putValusIntoMockedDbSet(Mock<DbSet<Module>> mockSet, List<Module> valuesToBeAssigned)
         {
-            PlatformManagement dbContext = new PlatformManagement();
-            _moduleRepository = new ModuleRepository(dbContext);
-            _testModule = CreateSUT();
+            var data = valuesToBeAssigned.AsQueryable();
+            mockSet.As<IQueryable<Module>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<Module>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<Module>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<Module>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
         }
 
-        [TestCleanup]
-        public void TearDown()
+        private DbSet<Module> mockDbSet()
         {
-            _moduleRepository = null;
+            expected_modules = new List<Module>
+            {
+                InitModule("eeeaccd6-f995-4a75-a5ce-02ee88bddab5"),
+                InitModule("e6d70219-b169-4d74-8b30-4242b218a744")
+            };
+            expected_updated_modules = new List<Module>(expected_modules);
+            expected_updated_modules[1].Description = "test";
+            var data = expected_modules.AsQueryable();
+            var mockSet = new Mock<DbSet<Module>>();
+            putValusIntoMockedDbSet(mockSet, expected_modules);
+            mockSet.Setup(d => d.Add(It.IsAny<Module>())).Callback<Module>((s) => data.Append(s));
+            mockSet.Setup(d => d.Update(It.IsAny<Module>())).Callback<Module>((s) => putValusIntoMockedDbSet(mockSet, expected_updated_modules));
+            return mockSet.Object;
+        }
+
+        private IPlatformManagement CreateSUT()
+        {
+            var mockPlatformManagement = new Mock<IPlatformManagement>();
+            var modules = mockDbSet();
+            mockPlatformManagement.Setup(m => m.Modules).Returns(modules);
+            mockPlatformManagement.Setup(d => d.Set<Module>()).Returns(modules);
+            return mockPlatformManagement.Object;
         }
 
         [TestMethod]
         public void When_TestModuleIsCreated_ShouldReturnItByFindAll()
         {
+            IPlatformManagement platformManagement = CreateSUT();
+            ModuleRepository moduleRepository = new ModuleRepository(platformManagement);
 
-            _testModule = _moduleRepository.Create(_testModule);
+            var new_module = InitModule(null);
 
-            List<Module> all = _moduleRepository.GetAll().ToList();
-            all.Should().Contain(_testModule);
+            moduleRepository.Create(new_module).Should().Be(new_module);
         }
 
         [TestMethod]
         public void When_TestModuleIsUpdated_ShouldReturnitByFindAll()
         {
-
-            _testModule.Description = "Updated module";
-            _moduleRepository.Update(_testModule);
-
-            List<Module> all = _moduleRepository.GetAll().ToList();
-            all.Should().Contain(_testModule);
+            IPlatformManagement platformManagement = CreateSUT();
+            ModuleRepository moduleRepository = new ModuleRepository(platformManagement);
+            moduleRepository.Update(expected_updated_modules[1]);
+            moduleRepository.GetAll().ShouldBeEquivalentTo(expected_updated_modules);
         }
 
         [TestMethod]
-        public void When_SearchingByTestModuleId_ShouldReturnTestModule()
+        public void When_GetByIdIsCalled_Then_TheCorespondingModuleIsReturned()
         {
-            Module result = _moduleRepository.GetById(Guid.Parse("0842f5b0-0ea3-4e16-143a-08d424682a17"));
+            IPlatformManagement platformManagement = CreateSUT();
+            ModuleRepository moduleRepository = new ModuleRepository(platformManagement);
 
-            result.Id.Should().Be(_testModule.Id);
+            Module result = moduleRepository.GetById(Guid.Parse("eeeaccd6-f995-4a75-a5ce-02ee88bddab5"));
+            result.Should().Be(expected_modules[0]);
         }
 
         [TestMethod]
-        public void When_TestModuleIsDeleted_ShouldNotReturnItByFindAll()
+        public void When_GetAllIsCalled_Then_AllTheObjectsInDataBaseAreReturned()
         {
-            _moduleRepository.Delete(_testModule);
+            IPlatformManagement platformManagement = CreateSUT();
+            ModuleRepository moduleRepository = new ModuleRepository(platformManagement);
 
-            List<Module> all = _moduleRepository.GetAll().ToList();
-            all.Should().NotContain(_testModule);
+            List<Module> all = moduleRepository.GetAll().ToList();
+            all.Should().BeEquivalentTo(expected_modules);
         }        
     }
 }
