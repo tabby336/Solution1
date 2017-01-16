4d355b1f84199bb64569b5fd38394f0feb91dc4f
tifuivali@gmail.com <tifui@DESKTOP-87T4SSH>
2017-01-17 00:11:17 +0200

    Create interface for Anouncements #47

---
+++ Solution1/Web/Models/AnouncementViewModel/AnouncementViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using DataAccess.Models;
+
+namespace Web.Models.AnouncementViewModel
+{
+    public class AnouncementViewModel
+    {
+        public IList<Anouncement> Anouncements { get; set; }
+    }
+}
