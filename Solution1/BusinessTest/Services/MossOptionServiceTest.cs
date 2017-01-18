96c78e8a64ed10f900d63a315955ce8e5ebf327e
Alberto Alexandru Cieri <alberto.cieri@info.uaic.ro>
2017-01-19 01:06:44 +0200

    cleanup #21

--- Solution1/BusinessTest/Services/MossOptionServiceTest.cs
+++ Solution1/BusinessTest/Services/MossOptionServiceTest.cs
@@ -4,6 +4,7 @@ using System.Security.Cryptography.X509Certificates;
 using Business.Services;
 using Business.Services.Interfaces;
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore.Query.Expressions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
@@ -12,23 +13,6 @@ namespace BusinessTest
     [TestClass]
     public class MossOptionServiceTest
     {
-        [TestMethod]
-        public void When_SendOptionIsCalled_WithCorrectParameters_StreamIsCalledToWrite()
-        {
-            var wasCalled = false;
-            var moqStream = new Mock<Stream>();
-
-            var businessMossOptions = new Mock<IMossOptionService>();
-            /*
-            businessMossOptions.Setup(x => x.SendOption("", "la", moqStream.Object)).
-                Callback<Stream>(
-                    xq => moqStream.Setup(x => x.Write(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<int>())).
-                        Callback((byte[] bytes, int offs, int c) =>
-                        {
-                            wasCalled = true;
-                        }));
-                        */
-        }
 
         [TestMethod]
         public void When_SendOptionIsCalled_WithNullParameters_NullArgumentExceptionIsThrown()
