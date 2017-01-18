5cd4e50b4b6bac751fa4ee80459c1945a24e7e6f
Marian FX <marian.focsa@outlook.com>
2017-01-18 20:24:29 +0200

    Updated Home View and Controller #41

--- Solution1/Web/Controllers/HomeController.cs
+++ Solution1/Web/Controllers/HomeController.cs
@@ -11,21 +11,6 @@ namespace Web.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View();
